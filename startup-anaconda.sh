ecec38c10cb8dc62a86eb3832628a259cc561cc0
George Gatheca <gatheca.george@gmail.com>
2024-09-23 13:21:37 +0100

    remove not needed file

--- startup-anaconda.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ex
-
-# Start jupyter
-jupyter notebook --notebook-dir=/home/hadoop/notebooks --ip='*' --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
